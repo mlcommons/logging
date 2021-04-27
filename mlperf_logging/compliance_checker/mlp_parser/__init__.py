7f0d5aa8f3bb3d16267ab8b3cf1db4955bde9826
Shang Wang <66387198+shangw-nvidia@users.noreply.github.com>
2021-04-27 13:47:18 -0400

    mlp_parser bringup to 1.0. (#101)

--- mlperf_logging/compliance_checker/mlp_parser/__init__.py
+++ mlperf_logging/compliance_checker/mlp_parser/__init__.py
@@ -1,5 +1,7 @@
 from .ruleset_060 import parse_file as parse_file_060
 from .ruleset_070 import parse_file as parse_file_070
+from .ruleset_100 import parse_file as parse_file_100
+
 
 def parse_file(filename, ruleset='0.6.0'):
     if ruleset == '0.6.0':
@@ -7,6 +9,6 @@ def parse_file(filename, ruleset='0.6.0'):
     elif ruleset == '0.7.0':
         return parse_file_070(filename)
     elif ruleset == '1.0.0':
-        return parse_file_070(filename)
+        return parse_file_100(filename)
     else:
         raise Exception(f'Ruleset "{ruleset}" is not supported')
