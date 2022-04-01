e3d64a0d7e1850a8276c11507c982057a643b0a4
emizan <emizan@google.com>
2022-04-01 19:21:48 +0000

    Reverting change I did not mean to commit.

--- scripts/verify_for_v1.1_training.sh
+++ scripts/verify_for_v1.1_training.sh
@@ -2,5 +2,5 @@
 
 set -e
 python3 -m mlperf_logging.package_checker $1 training 1.1.0
-python3 -m mlperf_logging.result_summarizer $1 training 1.1.0 --csv summary.csv
+python3 -m mlperf_logging.result_summarizer $1 training 1.1.0
 
