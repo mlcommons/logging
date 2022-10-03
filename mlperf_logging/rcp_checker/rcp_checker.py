371df66d23dccf0385bb73948fd95a884037c627
Maanu Grover <maanug@nvidia.com>
2022-10-03 14:59:41 -0500

    print normalization factor

--- mlperf_logging/rcp_checker/rcp_checker.py
+++ mlperf_logging/rcp_checker/rcp_checker.py
@@ -372,7 +372,9 @@ class RCP_Checker:
             logging.info(" RCP Record: %s", rcp_record)
             logging.info(" Submission mean epochs: %.4f", mean_subm_epochs)
             if mean_subm_epochs < rcp_record["RCP Mean"]:
-                logging.info(" Submission mean epochs faster than RCP mean but within max speedup range. Score should be normalized. ")
+                mesg = " Submission mean epochs faster than RCP mean but within max speedup range. Score should be normalized by factor of {} / {} = {}"
+                mesg = mesg.format(rcp_record['RCP Mean'], mean_subm_epochs, rcp_record['RCP Mean'] / mean_subm_epochs)
+                logging.info(mesg)
             return(True)
         else:
             logging.info(" RCP Record: %s", rcp_record)
