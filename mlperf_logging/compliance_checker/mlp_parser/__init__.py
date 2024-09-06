8d569822ba5f6f21ea49b8b1cb5dcb891c8aa19f
michalm <michalm@nvidia.com>
2024-09-06 13:44:22 +0200

    Add support for compliance for 4.1

--- mlperf_logging/compliance_checker/mlp_parser/__init__.py
+++ mlperf_logging/compliance_checker/mlp_parser/__init__.py
@@ -7,6 +7,7 @@ from .ruleset_210 import parse_file as parse_file_210
 from .ruleset_300 import parse_file as parse_file_300
 from .ruleset_310 import parse_file as parse_file_310
 from .ruleset_400 import parse_file as parse_file_400
+from .ruleset_410 import parse_file as parse_file_410
 
 
 def parse_file(filename, ruleset='0.6.0'):
@@ -28,5 +29,7 @@ def parse_file(filename, ruleset='0.6.0'):
         return parse_file_310(filename)
     elif ruleset == '4.0.0':
         return parse_file_400(filename)    
+    elif ruleset == '4.1.0':
+        return parse_file_410(filename)
     else:
         raise Exception(f'Ruleset "{ruleset}" is not supported')
