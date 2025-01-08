ef7db050a06cfc204ee782c8d45c6768cd3f5c77
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2025-01-08 17:48:42 -0500

    Fix minor issues

--- mlperf_logging/package_checker/power_checker.py
+++ mlperf_logging/package_checker/power_checker.py
@@ -21,7 +21,7 @@ class PowerChecker:
         seen = set({})
         errors = []
         for e in l:
-            if e < 0 or e >(n-1) or e in seen:
+            if e < 0 or e > (n-1) or e in seen:
                 return False
 
         return True
@@ -34,7 +34,7 @@ class PowerChecker:
                 counter[e] += 1
             else:
                 counter[e] = 1
-        max_equals = max(counter, counter.get)
+        max_equals = max(counter, key = counter.get)
         for i, e in enumerate(l):
             if e != max_equals:
                 errors.append(i)
@@ -55,8 +55,8 @@ class PowerChecker:
                 power_files = os.listdir(power_result_folder)
                 node_results  = [file for file in power_files if file.startswith("node")]
                 sw_results  = [file for file in power_files if file.startswith("sw")]
-                node_idx  = [os.path.splitext(os.path.basename(file))[1] for file in node_results]
-                sw_idx  = [os.path.splitext(os.path.basename(file))[1] for file in node_results]
+                node_idx  = [int(os.path.splitext(os.path.basename(file))[0].split('_')[-1]) for file in node_results]
+                sw_idx  = [int(os.path.splitext(os.path.basename(file))[0].split('_')[-1]) for file in sw_results]
 
                 if len(power_files) > len(node_results) + len(sw_results):
                     logging.warning("Detected %d total files in directory %s, but some do not conform", len(power_files), power_result_folder)
@@ -65,7 +65,7 @@ class PowerChecker:
                     logging.warning("Bad naming of node power files in directory %s, expected to be node_x with x in range [0, %d]", power_result_folder, len(node_results)-1)
                     errors_found += 1
                     errors_set.add(result_name)
-                if not self.check_range(sw_idx, len(sw_lens)):
+                if not self.check_range(sw_idx, len(sw_results)):
                     logging.warning("Bad naming of sw power files in directory %s, expected to be sw_x with x in range [0, %d]", power_result_folder, len(sw_results)-1)
                     errors_found += 1
                     errors_set.add(result_name)
@@ -80,11 +80,17 @@ class PowerChecker:
 
         result_names = [os.path.splitext(os.path.basename(result_file))[0] for result_file in result_files]
 
-        valid, errors = self.check_mode(node_results)
+        valid, errors = self.check_equals(node_lens)
         node_errors = set([result_names[error] for error in errors])
-
-        valid, errors = self.check_mode(sw_results)
+        for error_result in [result_names[error] for error in errors]:
+            logging.warning("Inconsistent number of nodes in directory %s/%s", power_folder, error_result)
+            logging.warning("Directory %s/%s does not comply", power_folder, error_result)
+        
+        valid, errors = self.check_equals(sw_lens)
         sw_errors = set([result_names[error] for error in errors])
+        for error_result in [result_names[error] for error in errors]:
+            logging.warning("Inconsistent number of sw in directory %s/%s", power_folder, error_result)
+            logging.warning("Directory %s/%s does not comply", power_folder, error_result)
         
         errors_set = errors_set | node_errors | sw_errors
         return errors_found == 0, errors_set
