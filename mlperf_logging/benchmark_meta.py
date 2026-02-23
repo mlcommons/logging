91e50bfd4d9fae455cbe37b1770963c8a1ec8bb3
Su Ann Chong <suachong@amd.com>
2026-02-23 09:00:03 -0600

    update logging for gpt-oss-20b

--- mlperf_logging/benchmark_meta.py
+++ mlperf_logging/benchmark_meta.py
@@ -23,6 +23,7 @@ _ALL_RESULT_FILE_COUNTS = {
         # TODO: Update with official values
         'llama31_8b': 10,
         'flux1': 10,
+        'gpt_oss_20b': 10,
     },
     
     'hpc' : {
@@ -161,7 +162,8 @@ _ALL_ALLOWED_BENCHMARKS = {
         'dlrm_dcnv2',   
         'flux1',
         'llama2_70b_lora',
-        'llama31_405b'
+        'llama31_405b',
+        'gpt_oss_20b'
     ]    
     },
     
